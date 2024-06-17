2aff5fb6ff6fea9d1b292911f6796959a96ba0f8
Deyf3r <133782394+Deyf3r@users.noreply.github.com>
2024-06-16 23:44:22 -0400

    Add files via upload

---
+++ Sistema_Escolar.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sistema_Escolar.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
