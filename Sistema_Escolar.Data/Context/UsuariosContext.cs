437c549c51e3423dc230eab4d46d828254c866b9
jimmyguzmant04 <jimmyguzmant04@gmail.com>
2024-06-29 00:28:20 -0400

    Creacion tabla usuarios

---
+++ Sistema_Escolar.Data/Context/UsuariosContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Sistema_Escolar.Data.Entities;
+
+namespace Sistema_Escolar.Data.Context
+{
+    public class UsuariosContext : DbContext
+    {
+        public UsuariosContext(DbContextOptions<UsuariosContext> options): base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("EscuelaDB");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Usuarios> Usuarios { get; set; }    
+        
+    }
+}
---
+++ Sistema_Escolar.Data/Repositories/Db/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using Sistema_Escolar.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Escolar.Data.Repositories.Db
+{
+    public interface IUsuarioRepository
+    {
+        void Agregar(Usuarios usuarios);
+        void Actualizar(Usuarios usuarios);
+        void Remover(Usuarios usuarios);
+        List<Usuarios> TraerTodos();
+        Usuarios ObtenerPorId(int IdUsuario);
+    }
+}
